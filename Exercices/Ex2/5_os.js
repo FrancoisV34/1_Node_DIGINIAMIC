410c71da961beb269519b47cb8e195ce0225cf83
François <francois.vittecoq@osteopathe-eso.fr>
2025-12-10 11:40:40 +0100

    ex 5 ok

--- Exercices/Ex2/5_os.js
+++ Exercices/Ex2/5_os.js
@@ -2,3 +2,21 @@
 // Cette fonction affichera un message différend pour Windows, macOS et Linux
 // Appelez la fonction pour la tester
 // Il faudra trouver quel est la chaîne de retour pour chacun des systèmes
+
+const os = require('os');
+
+function displayOsMessage() {
+  const platform = os.platform();
+
+  if (platform === 'win32') {
+    console.log('Vous utilisez Windows.');
+  } else if (platform === 'darwin') {
+    console.log('Vous utilisez macOS.');
+  } else if (platform === 'linux') {
+    console.log('Vous utilisez Linux.');
+  } else {
+    console.log("Système d'exploitation non reconnu.");
+  }
+}
+
+displayOsMessage();
